a083fe88965aa8058d0151bbcfb0c633a3e4bf1d
colinplancich <colin.plancich@gmail.com>
2022-08-01 16:28:50 -0700

    Adding starting files w/ pseudocode

---
+++ index.js
@@ -0,0 +1,19 @@
+// Packages needed to use this application
+const fs = require ('fs');
+const inquirer = require('inquirer');
+
+// write file
+//  ask the user for data
+// generate .md file
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialze app
+init();
